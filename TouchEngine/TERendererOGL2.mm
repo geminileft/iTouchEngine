35fcb9c7b1a68ba3e8a898e686696631259e0fe1
Matt Galaviz <matthew.galaviz@gmail.com>
2011-11-24 00:57:31 -0800

    weird order of matrix multiplication

--- TouchEngine/TERendererOGL2.mm
+++ TouchEngine/TERendererOGL2.mm
@@ -67,7 +67,7 @@ TERendererOGL2::TERendererOGL2(CALayer* eaglLayer) {
 void TERendererOGL2::render() {
     glClear(GL_COLOR_BUFFER_BIT);
     renderBasic();
-    //renderTexture();
+    renderTexture();
     glBindRenderbufferOES(GL_RENDERBUFFER_OES, mRenderBuffer);
     [mContext presentRenderbuffer:GL_RENDERBUFFER_OES];
 }
