9428cfba727c198a260762884f714e87231dc42c
Matt Galaviz <matthew.galaviz@gmail.com>
2011-11-24 11:41:32 -0800

    isolating view matrix

--- TouchEngine/TERendererOGL2.mm
+++ TouchEngine/TERendererOGL2.mm
@@ -185,7 +185,7 @@ uint TERendererOGL2::switchProgram(String programName) {
     float view[16];
     TEUtilMatrix::setFrustum(&proj[0], -0.5, 0.5f, -0.75f, 0.75f, 0.5f, 2.0f);
     TEUtilMatrix::setIdentity(&view[0]);
-    TEUtilMatrix::setTranslate(&view[0], 0, 0, -1.0f);
+    //TEUtilMatrix::setTranslate(&view[0], 0, 0, -2.0f);
     uint mProjHandle  = TERendererOGL2::getUniformLocation(program, "uProjectionMatrix");
     uint mViewHandle = TERendererOGL2::getUniformLocation(program, "uViewMatrix");
     glUniformMatrix4fv(mProjHandle, 1, GL_FALSE, &proj[0]);
